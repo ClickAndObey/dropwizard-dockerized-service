f1f7b6fc4ba65a4e562c6a64202c92465e0c68be
drush211 <thedrush21@gmail.com>
2021-02-06 09:49:48 -0600

    Update documentation and use Junit 5 instead of 4.

--- src/test/java/clickandobey/dockerized/webservice/PlaceHolderTest.java
+++ src/test/java/clickandobey/dockerized/webservice/PlaceHolderTest.java
@@ -3,7 +3,7 @@
  */
 package clickandobey.dockerized.webservice;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
 public class PlaceHolderTest {
     @Test public void testPlaceholder() {
